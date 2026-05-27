f866fb4f192ce9a6e4a7c532a22268c152ce09f1
heyneil09 <104291572+heyneil09@users.noreply.github.com>
2026-05-27 22:48:48 +0200

    Initialize Simulation with configuration file

--- part_1_disease/simulation/main.cpp
+++ part_1_disease/simulation/main.cpp
@@ -4,7 +4,7 @@
 
 int main() {
   std::cout << "Loading Disease Simulation..." << std::endl;
-  Simulation sim = Simulation();
+  Simulation sim = Simulation("../disease_in.ini");
   sim.start();
   return 0;
 }
